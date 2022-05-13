ab73257596cfe7ccd110faaa7bd60efd4340b764
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-05-13 16:01:39 +0400

    Add files via upload

---
+++ Task_34_5_1.cpp
@@ -0,0 +1,12 @@
+﻿// Task_34_5_1.cpp: определяет точку входа для приложения.
+//
+
+#include "Task_34_5_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
